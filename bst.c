988ecbebc5c6874f98524d4f2af9ffc2ca0b1cc6
Harry Parsons <harryjohnparsons@outlook.com>
2022-10-20 15:39:28 +0100

    0.5

--- bst.c
+++ bst.c
@@ -19,35 +19,68 @@ Node *addNode(Node *root, int value)
     root->value = value;
     root->left = NULL;
     root->right = NULL;
-    printf("Node not present\n");
-    printf("%d\n", root->value);
+    //printf("Node not present\n");
+   // printf("%d\n", root->value);
   }
   else if (value > root->value)
   {
     root->left = addNode(root->left, value);
-    printf("Node present - %d inserted to the left\n", value);
+    //printf("Node present - %d inserted to the left\n", value);
   }
   else if (value == root->value)
   {
-    printf("DUPE");
+    //printf("DUPE");
   }
   else
   {
     root->right = addNode(root->right, value);
-    printf("Node present - %d inserted to the right\n", value);
+    //printf("Node present - %d inserted to the right\n", value);
   }
   return root;
 }
 
+Node * find_Pred(Node * root, int value){
+
+  if(root == NULL){
+    return NULL;
+  }
+  if (root->value == value){
+
+    if(root->left != NULL){
+      Node * temp = root->left;
+      while(temp->right != NULL){
+        temp = temp->right;
+      }
+      return temp;
+    }
+    else if(root->right != NULL){
+      Node * temp = root->right;
+      while(temp->left != NULL){
+        temp = temp->left;
+      }
+      return temp;
+    }
+  }
+  else if (root->value > value){
+    root->right = find_Pred(root->right, value);
+  }
+  else{
+    root->left = find_Pred(root->left,value);
+  }
+  
+}
+
 Node *removeNode(Node *root, int value)
 {
-  if(root->value == value){ 
+  if(root->value == value ){ 
    // printf("%d found\n", value);
+    Node * rmValue = root;
+
+
     if(root->left == NULL && root->right == NULL){ // 0 children
-        printf("%d is deleted\n", root->value);
-        Node * temp = root;
+        printf("%d is deleted\n", root->value);      
         free(root);
-        return temp;
+        
     }
     else if(root->right == NULL){ // 1 child left
         printf("%d is deleted\n", root->value);
@@ -61,30 +94,61 @@ Node *removeNode(Node *root, int value)
         free(root);
         return temp;
     }
-    
     else{
-      // 2 children
+      //found at root
+      // 2 children need to find the the closest value below the deleted node and swap with rmValue
+      Node * closest = find_Pred(root,value);
+      printf("%d swaped with %d\n", closest->value, root->value);
+      
+      //closest -> left = rmValue->left;
+      //closest -> right =rmValue->right;
+      root->value = closest->value;
+      //closest = NULL;
+      free(closest);
+      return root;
     }
 
+  }
     
+  else if(root->left->value == value && root->left->right != NULL && root->left->left != NULL){
+    // 2 children need to find the the closest value below the deleted node and swap with rmValue
+      Node * closest = find_Pred(root->left,value);
+      printf("2: %d swaped with %d\n", closest->value, root->left->value);
+      
+      closest->left = root->left->left;
+      //printf("%d assigned\n", root->value);
+     
+      closest->right = root->left->right;
+       //printf("%d assigned\n", root->left->right->value);
+      root->left = closest;
+      //closest = NULL;
+       free(closest);
+      
+  }
+  else if (root->right->value == value && root->right->right != NULL && root->right->left != NULL){
+    printf("Here");
   }
   else if (value > root->value && root->left != NULL){
-    removeNode(root->left,value);
+    printf("Looking down left\n");
+    root->left = removeNode(root->left,value);
   }
   else if(value < root->value && root->right != NULL){
-    removeNode(root->right,value);
+      printf("Loooking down left\n");
+      root->right = removeNode(root->right,value);
   }
   else{
-    return root;
+    
   }
-  return root;
- 
+  
+ return root;
 }
+
+
 
 void displaySubtree(Node *N)
 {
 
-  if (N != NULL && N->value !=0)
+  if (N != NULL && N->value != 0)
   {
     displaySubtree(N->right);
     printf("%d\n", N->value);
@@ -117,27 +181,31 @@ int numberLeaves(Node *N)
   }
 }
 
-/*Node *removeSubtree(Node *root, int value)
+Node *removeSubtree(Node *root, int value)
 {
+ 
+  if(root->left->value == value) {
+      free(root->left);
+      //return root;
+  }
+  else if(root->right->value == value){
+    free(root->right);
+    //return root;
+  }
+  else if(value < root->value && root != NULL){
+    root->right = removeSubtree(root->right,value);
+  }
+  else if(value > root->value && root != NULL){
+    root->left = removeSubtree(root->left,value);
+  }
+  else{
+    return NULL;
+  }
+  
+  return root;
 
-  Node *temp = searchBST(root, value);
-  if (temp != NULL)
-  {
-    temp->value = 0;
-    temp->left = NULL;
-    temp->right = NULL;
-    printf("%d and the subtree below was removed \n", value);
-    return temp;
-    // cannot set to NULL
-  }
-  else
-  {
-    printf("Not present in tree");
-    // return root;
-  }
-  return temp;
 }
-*/
+
 int nodeDepth(Node *R, Node *N)
 {
 
@@ -186,45 +254,48 @@ Node *searchBST(Node *root, int value)
   //return NULL;
 }
 
-//int smallestvalue
+
 int main()
 {
   Node *n, *b;
   n = NULL;
 
-  n = addNode(NULL, 6);
-  n = addNode(n, 9);
-  n = addNode(n, 7);
-  n = addNode(n, 4);
-  n = addNode(n, 2);
-  // n = addNode(n, 4);
-  n = addNode(n, 5);
- // n = addNode(n, 10);
- // b = addNode(NULL, 5);
-  /*
-  n = addNode(n, 10);
-  n = addNode(n, 23490873);
-  n = addNode(n, 213);
-  n = addNode(n, 3);
-  n = addNode(n, 1);
-*/
-  // n = removeNode(n, 4);
-  // n = removeSubtree(n,4);
+  n = addNode(NULL, 50);
+  n = addNode(n, 40);
+  n = addNode(n, 45);
+  n = addNode(n, 35);
+  n = addNode(n, 60);
+  n = addNode(n, 65);
+  n = addNode(n, 66);
+  n = addNode(n, 64);
+  n = addNode(n, 55);
+  n = addNode(n, 56);
+  n = addNode(n, 54);
+ // n = addNode(n,36);
 
-  // printf("%d\n", n->value);
-  // printf("%d\n", n->left->value);
-  // printf("%d\n", n->left->right->value);
-  // printf("%d\n", n->right->value);
-  // printf("%d\n", n->right->right->value);
-  // printf("%d", n->right->left->value);
 
-  // displaySubtree(n);
   // printf("%d depth\n", nodeDepth(n, b));
-   //printf("Found: %d", numberLeaves(n));
+  //printf("Found: %d", numberLeaves(n));
  // n = removeNode(n,10);
-  n = removeNode(n,9);
-  //displaySubtree(n);
+
+  //n = removeNode(n,5);
+   n = removeNode(n,60);
+ //n = removeSubtree(n,65);
+/*
+  printf("%d\n" , n->value);
+  printf("%d\n" , n->left->value);
+  printf("%d\n" , n->right->value);
+  printf("%d\n" , n->right->right->value);
+  printf("%d\n" , n->left->right->right->right->value);
+*/
+ // printf("%d\n" , n->right->->value);
+   //printf("%d\n" , n->left->right->value);
+   printf("Break\n");
+   displaySubtree(n);
+   //n = addNode(n,65);
+  // displaySubtree(n);
   //free(n);
 
   return 0;
 }
+ 
